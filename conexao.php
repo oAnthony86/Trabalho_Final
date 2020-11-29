807ddec2a4049323ed13a6772e185ad676028474
oAnthony86 <drabyyt@gmail.com>
2020-11-29 18:45:28 -0300

    Index
    index e arq de conexao

---
+++ conexao.php
@@ -0,0 +1,18 @@
+<?php
+
+$username= "root";
+$password= '';
+$banco= "northwind";
+$host= "localhost";
+
+try{
+    $conexao = new PDO(
+    "mysql:host={$host}; dbname={$banco}", $username, 
+    $password);
+        $conexao->setAttribute( PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Erro: {$e->getMessage()}";
+}
+
+?>
